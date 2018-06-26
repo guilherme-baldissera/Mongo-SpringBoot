1007e206cefed8a85ecb19076b9b551702debf13
Guilherme Augusto Moreira Baldissera <gbaldissera@brdlgbaldissera.daitan.com>
2018-06-26 16:51:07 -0300

    group entity fixed

--- src/main/java/com/daitangroup/mysql/entities/Group.java
+++ src/main/java/com/daitangroup/mysql/entities/Group.java
@@ -1,5 +1,7 @@
 package com.daitangroup.mysql.entities;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
@@ -15,20 +17,31 @@ public class Group implements Serializable {
     @Id
     @Getter
     @Setter
-    String id;
+    private String id;
+    @Getter
+    @Setter
+    @NotNull
+    private User userAdmin;
     @Getter
     @Setter
     @NotNull
     @NotEmpty
-    User userAdmin;
+    private List<User> guests;
     @Getter
     @Setter
     @NotNull
     @NotEmpty
-    List<User> guests;
-    @Getter
-    @Setter
-    @NotNull
-    @NotEmpty
-    String name;
+    private String name;
+
+    public Group(String id) {
+        this.id = id;
+    }
+
+    @JsonCreator
+    public Group(@JsonProperty("userAdmin") @NotNull User userAdmin, @NotNull @NotEmpty @JsonProperty("guests") List<User> guests,
+                 @JsonProperty("name") @NotNull @NotEmpty String name) {
+        this.userAdmin = userAdmin;
+        this.guests = guests;
+        this.name = name;
+    }
 }
